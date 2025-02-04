a14b1df0943ea3cf4530177b49a2c5c034fe6e23
Snetas <snetas927@gmail.com>
2025-02-04 22:31:09 +0000

    This is my first BBD framework

---
+++ src/test/java/steps/features/LoginSteps.java
@@ -0,0 +1,26 @@
+package steps.features;
+
+import io.cucumber.java.en.*;
+
+public class LoginSteps {
+
+    @Given("the user is on the login page")
+    public void userIsOnLoginPage() {
+        System.out.println("User is on login page");
+    }
+
+    @When("the user enters valid username and password")
+    public void enterValidCredentials() {
+        System.out.println("User enters valid credentials");
+    }
+
+    @When("clicks on the login button")
+    public void clickLoginButton() {
+        System.out.println("User clicks on login button");
+    }
+
+    @Then("the user should be redirected to the homepage")
+    public void verifyHomepage() {
+        System.out.println("User is redirected to homepage");
+    }
+}
